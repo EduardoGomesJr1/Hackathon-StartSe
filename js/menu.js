d47ed3d3be947f6205550a5bd86a58e16acd3672
EduardoGomesJr1 <106855814+EduardoGomesJr1@users.noreply.github.com>
2022-08-28 16:20:26 -0300

    Add files via upload
    Update

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,17 @@
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
-
-function toggleMenu(event){
-if (event.type === 'touchstart') event.preventDefault();
-const containerNav = document.getElementById('container-nav');
-containerNav.classList.toggle('active');
-const active = containerNav.classList.contains('active')
-event.currentTarget.setAttribute('aria-expanded', active);
-    if (active){
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const containerNav = document.getElementById("container-nav");
+    containerNav.classList.toggle("active");
+    const active = containerNav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+    if (active) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
     } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
     }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
